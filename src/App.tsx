bd98ef48b69f78288609e1213e74b530fe6af047
lnara404 <naas0974@gmail.com>
2022-12-30 15:29:47 +0900

    :package: Add stylelint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import './App.css';
+import styled from '@emotion/styled';
+
+const StyledTitle = styled.h1`
+  text-align: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <h1>Design System Study🎨</h1>
+    <div className='App'>
+      <StyledTitle>Design System Study🎨</StyledTitle>
     </div>
   );
 }
